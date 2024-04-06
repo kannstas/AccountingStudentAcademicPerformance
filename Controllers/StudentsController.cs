6fcf35ed8190903938a4a49ad3f03900ca92b273
Анастасия Кулакова <k.annstas@gmail.com>
2024-04-06 19:59:14 +0300

    Добавьте файлы проекта.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,35 @@
+﻿using AccountingStudentAcademicPerformance.Data;
+using AccountingStudentAcademicPerformance.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AccountingStudentAcademicPerformance.Controllers
+{
+    public class StudentsController : Controller
+    {
+		private readonly AppDbContext _context;
+
+		public StudentsController(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			var students = _context.Students;
+			return View("Index", students);
+		}
+
+		[HttpPost]
+		public IActionResult AddStudent(Student student)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Students.Add(student);
+				_context.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View("Index", student);
+		}
+	}
+}
---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccountingStudentAcademicPerformance.Models
+{
+	public class Student
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+
+		[Required]
+		public int Score { get; set; }
+	}
+}
